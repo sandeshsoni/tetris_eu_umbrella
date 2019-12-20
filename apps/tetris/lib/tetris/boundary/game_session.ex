3778f123f5e7bc4af56556517dd9508972ffe1de
Sandesh Soni <hello@sandeshsoni.com>
2019-12-20 21:20:24 +0530

    move down shape every 500ms

--- apps/tetris/lib/tetris/boundary/game_session.ex
+++ apps/tetris/lib/tetris/boundary/game_session.ex
@@ -6,24 +6,16 @@ defmodule Tetris.Boundary.GameSession do
 
 
   def init(opts) do
-    # dummy game with a few tiles occupied
-
-    # IO.puts "############ opts"
-    # IO.puts inspect(opts)
-
-    # game = Game.new(%{game_id: self()})
     game = Game.new(opts)
     shape = Shape.new(:l_shape)
     custom_position_coordinates = {2, 5}
     shape_added_board = Board.add_shape(game.board, shape, custom_position_coordinates)
     game = %Game{game | board: shape_added_board}
 
+    Process.send_after(self(), :tick, 500)
+
     {:ok, game}
   end
-
-  # def start_link(args) do
-  #   GenServer.start_link(__MODULE__, args)
-  # end
 
   def handle_call(:get_state, _from, game_state) do
     {:reply, game_state}
@@ -49,8 +41,25 @@ defmodule Tetris.Boundary.GameSession do
   def stop do
   end
 
+  def handle_info(:tick, game_state) do
+    # GenServer.call(self(), {:move, :down})
+    # notify_game_changed(state, state)
+
+    IO.puts "tick"
+    state_after_move = GameLogic.move(game_state, :down)
+
+    if state_after_move.current_state == :finish do
+      # {:stop}
+    else
+      Process.send_after(self(), :tick, 500)
+    end
+
+    notify_game_changed(state_after_move, state_after_move)
+
+    {:noreply, state_after_move}
+  end
+
   defp notify_game_changed(state, next_state) do
-    # game_session_pid = state.game_id
     state_change_listener = state.state_change_listener
 
     Process.send(state_change_listener, {:state_change, next_state}, [])
