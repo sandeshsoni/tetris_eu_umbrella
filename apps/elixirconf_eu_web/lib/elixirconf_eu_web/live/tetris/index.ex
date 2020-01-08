d81968fe18497e0f5e1dc10017ac653155287085
Sandesh Soni <hello@sandeshsoni.com>
2020-01-09 00:07:54 +0530

    comment old code

--- apps/elixirconf_eu_web/lib/elixirconf_eu_web/live/tetris/index.ex
+++ apps/elixirconf_eu_web/lib/elixirconf_eu_web/live/tetris/index.ex
@@ -14,11 +14,10 @@ defmodule  ElixirconfEuWeb.Tetris.Index do
     {:ok, new_socket}
   end
 
-  def handle_event({:state_change, new_state}, socket) do
-
-    IO.puts "hanle event"
-    {:noreply, socket}
-  end
+  # def handle_event({:state_change, new_state}, socket) do
+  #   IO.puts "hanle event"
+  #   {:noreply, socket}
+  # end
 
   # def handle_info(msg, state) do
   #   # Logger.error("Unhandled message -> module: #{__MODULE__}, msg: #{inspect msg}, state: #{inspect state}")
@@ -89,66 +88,66 @@ defmodule  ElixirconfEuWeb.Tetris.Index do
 
   ##################### old code to remove ###################
 
-  defp initial_stated(socket) do
-    game = Game.new(%{})
-    shape = Shape.new(:l_shape)
-    custom_position_coordinates = {2, 5}
-    shape_added_board = Board.add_shape(game.board, shape, custom_position_coordinates)
-    game = %Game{game | board: shape_added_board}
+  # defp initial_stated(socket) do
+  #   game = Game.new(%{})
+  #   shape = Shape.new(:l_shape)
+  #   custom_position_coordinates = {2, 5}
+  #   shape_added_board = Board.add_shape(game.board, shape, custom_position_coordinates)
+  #   game = %Game{game | board: shape_added_board}
 
-    assign(socket,
-      # game: game,
-      game_over: game.game_over,
-      board: game.board,
-      active_shape: game.active_shape,
-      next_shape: game.next_shape,
-      score: game.score,
-      game_over: game.game_over,
-      offset_x: game.offset_x,
-      offset_y: game.offset_y,
-      player_name: "somebody",
-      new_game: true,
-      speed: 600
-    )
-  end
+  #   assign(socket,
+  #     # game: game,
+  #     game_over: game.game_over,
+  #     board: game.board,
+  #     active_shape: game.active_shape,
+  #     next_shape: game.next_shape,
+  #     score: game.score,
+  #     game_over: game.game_over,
+  #     offset_x: game.offset_x,
+  #     offset_y: game.offset_y,
+  #     player_name: "somebody",
+  #     new_game: true,
+  #     speed: 600
+  #   )
+  # end
 
-  defp restart_game(socket, game_mode) do
-    game = Game.new(game_mode)
+  # defp restart_game(socket, game_mode) do
+  #   game = Game.new(game_mode)
 
-    assign(socket,
-      # game: game,
-      score: game.score,
-      active_shape: game.active_shape,
-    )
-  end
+  #   assign(socket,
+  #     # game: game,
+  #     score: game.score,
+  #     active_shape: game.active_shape,
+  #   )
+  # end
 
-  def render(%{new_game: true} = assigns) do
+  def render(assigns) do
     # IO.puts inspect(assigns)
     ElixirconfEuWeb.TetrisView.render("tetris-game.html", assigns)
   end
 
 
-  def handle_event("game_submit", %{
-        "player-name" => player_name,
-        "game-mode" => game_mode} = form, socket) do
-    mode = String.to_atom(game_mode)
-    game = Game.new(mode)
+  # def handle_event("game_submit", %{
+  #       "player-name" => player_name,
+  #       "game-mode" => game_mode} = form, socket) do
+  #   mode = String.to_atom(game_mode)
+  #   game = Game.new(mode)
 
-    #config maintaintains game board
+  #   #config maintaintains game board
 
 
-    {:noreply,
-     assign(socket,
-       player_name: player_name,
-       board: game.board,
-       active_shape: game.active_shape,
-       score: game.score,
-       # game: game,
-       game_over: game.game_over,
-       speed: game.speed,
-       new_game: false
-     )
-    }
-  end
+  #   {:noreply,
+  #    assign(socket,
+  #      player_name: player_name,
+  #      board: game.board,
+  #      active_shape: game.active_shape,
+  #      score: game.score,
+  #      # game: game,
+  #      game_over: game.game_over,
+  #      speed: game.speed,
+  #      new_game: false
+  #    )
+  #   }
+  # end
 
 end
